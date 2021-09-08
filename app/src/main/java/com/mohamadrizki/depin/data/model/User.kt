6e1168d7e1361a8535c521fdeca2d66fb0bd929a
svenster18 <mohamadrizki8@gmail.com>
2021-09-08 16:28:34 +0700

    add name in user data class

--- app/src/main/java/com/mohamadrizki/depin/data/model/User.kt
+++ app/src/main/java/com/mohamadrizki/depin/data/model/User.kt
@@ -2,6 +2,7 @@ package com.mohamadrizki.depin.data.model
 
 data class User(
     val userId: String,
+    val name: String,
     val email: String,
     val password: String,
     val idNumber: String,
