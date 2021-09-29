7d35a37e06e41061113154a0e084f099ceefbe66
svenster18 <mohamadrizki8@gmail.com>
2021-09-29 12:22:27 +0700

    fix some visual bugs and code spinner

--- app/src/main/java/com/mohamadrizki/depin/ui/pemesanan/PemesananActivity.kt
+++ app/src/main/java/com/mohamadrizki/depin/ui/pemesanan/PemesananActivity.kt
@@ -2,6 +2,8 @@ package com.mohamadrizki.depin.ui.pemesanan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.mohamadrizki.depin.R
 import com.mohamadrizki.depin.databinding.ActivityPemesananBinding
 import com.mohamadrizki.depin.ui.home.HomeFragment
 
@@ -14,5 +16,16 @@ class PemesananActivity : AppCompatActivity() {
         val shipClass = intent.getStringExtra(HomeFragment.EXTRA_SHIP_CLASS)
 
         binding.tvShipClassContainer.text = shipClass
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.city,
+            android.R.layout.simple_spinner_item,
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            binding.spFrom.adapter = adapter
+        }
     }
 }
