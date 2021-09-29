213d276cc8a7add4324de80f5a2568d78125b525
svenster18 <mohamadrizki8@gmail.com>
2021-09-29 11:04:38 +0700

    design pemesanan activity layout and apply navigation from home to pemesanan

--- app/src/main/java/com/mohamadrizki/depin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mohamadrizki/depin/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.mohamadrizki.depin.R
 import com.mohamadrizki.depin.databinding.FragmentHomeBinding
 
@@ -38,8 +39,35 @@ class HomeFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ibEconomy.setOnClickListener {
+            book(view, ECONOMY)
+        }
+        binding.ibBusiness.setOnClickListener {
+            book(view, BUSINESS)
+        }
+        binding.ibExecutive.setOnClickListener {
+            book(view, EXECUTIVE)
+        }
+    }
+
+    fun book(view: View, shipClass: String) {
+        val mBundle = Bundle()
+        mBundle.putString(EXTRA_SHIP_CLASS, shipClass)
+        view.findNavController().navigate(R.id.action_navigation_home_to_pemesananActivity, mBundle)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        const val ECONOMY = "economy"
+        const val BUSINESS = "business"
+        const val EXECUTIVE = "executive"
+
+        const val EXTRA_SHIP_CLASS = "extra_ship_class"
     }
 }
---
+++ app/src/main/java/com/mohamadrizki/depin/ui/pemesanan/PemesananActivity.kt
@@ -0,0 +1,17 @@
+package com.mohamadrizki.depin.ui.pemesanan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mohamadrizki.depin.R
+import com.mohamadrizki.depin.databinding.ActivityPemesananBinding
+import com.mohamadrizki.depin.ui.home.HomeFragment
+
+class PemesananActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityPemesananBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val shipClass = intent.getStringExtra(HomeFragment.EXTRA_SHIP_CLASS)
+    }
+}
